4484bb9b671626a84533f9a1eedde4ee71dce4f8
DilahNURFADILAAMIR <dilanurfadilah689@gmail.com>
2025-09-10 18:27:45 +0800

    Day3.java

---
+++ Day3.java
@@ -0,0 +1,38 @@
+
+public class Day3 {
+    public static void main(String[] args) {
+
+        /*
+         Multi line comment
+         Biasanya digunakan untuk memberikan keterangan panjang
+         agar orang lain lebih mudah memahami program.
+        */
+        System.out.println("--- Multi line Comment ---"); // Menampilkan judul bagian komentar
+
+        String penyanyi = "Taylor Swift";
+        String genre = "Pop";
+
+        System.out.printf("Penyanyi: %-12s | Genre: %s%n", penyanyi, genre);
+        // Taylor Swift adalah penyanyi pop terkenal
+
+        System.out.print("Album favorit saya adalah: ");
+        System.out.println("1989");
+        // Album ini dirilis tahun 2014 dan sangat populer
+
+        /*
+         Taylor Swift adalah seorang penyanyi dan penulis lagu asal Amerika.
+         Ia debut pada tahun 2006 dengan lagu-lagu bergenre country,
+         lalu semakin populer setelah merambah ke musik pop.
+
+         Hingga kini, Taylor Swift dikenal dengan album-album suksesnya seperti:
+         - Fearless
+         - Red
+         - 1989
+         - Reputation
+         - Lover
+         - Folklore
+         - Evermore
+         - Midnights
+        */
+    }
+}
