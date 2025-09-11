6ca4f2a9f6b05803763876bd787769df9350781c
DilahNURFADILAAMIR <dilanurfadilah689@gmail.com>
2025-09-11 18:02:55 +0800

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,29 @@
+public class Day4 {
+
+        public static void main(String[] args) {
+
+            // Contoh penggunaan \n (new line)
+            System.out.print("Halo, ini baris pertama.\nIni baris kedua.\nIni baris ketiga.\n");
+
+            // Contoh penggunaan \t (tab)
+            System.out.println("Nama\t\t: Dila");
+            System.out.println("Pekerjaan\t: Mahasiswi");
+            System.out.println("Hobi\t\t: Membaca");
+
+            // Contoh penggunaan \" (double quote)
+            System.out.println("Dia berkata: \"Belajar Java itu seru!\"");
+
+            // Contoh penggunaan \' (single quote)
+            System.out.println("Karakter tunggal: \'A\'");
+
+            // Contoh penggunaan \\ (backslash)
+            System.out.println("Path file: C:\\Users\\Public\\Documents");
+
+            // Contoh penggunaan \b (backspace)
+            System.out.println("Halo Dunia!!!\b\b\b"); // 3 kali backspace
+
+            // Contoh penggunaan \r (carriage return)
+            System.out.print("Belajar Java\rMulai Lagi");
+
+        }
+    }
