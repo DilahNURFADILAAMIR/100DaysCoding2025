f1d762f8fdb0216b58699d5e265702e40db84d61
DilahNURFADILAAMIR <dilanurfadilah689@gmail.com>
2025-09-12 16:02:05 +0800

    Day5.java

---
+++ Day5.java
@@ -0,0 +1,21 @@
+public class Day5 {
+    public static void main(String[] args) {
+        // 1. byte (8-bit)
+        byte umur = 19;
+
+        // 2. short (16-bit)
+        short jumlahPendudukDesa = 15000;
+
+        // 3. int (32-bit)
+        int gajiBulanan = 5500000;
+
+        // 4. long (64-bit)
+        long jumlahPenggunaInstagram = 2000000000L;
+
+        // Mencetak nilai variabel
+        System.out.println(umur);
+        System.out.println(jumlahPendudukDesa);
+        System.out.println(gajiBulanan);
+        System.out.println(jumlahPenggunaInstagram);
+    }
+}
