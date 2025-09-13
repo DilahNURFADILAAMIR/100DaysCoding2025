d7ca4299840670ed0eba37f118afa9dd2ebf81e5
DilahNURFADILAAMIR <dilanurfadilah689@gmail.com>
2025-09-13 21:16:47 +0800

    Day6.java

---
+++ Day6.java
@@ -0,0 +1,13 @@
+public class Day6 {
+
+        public static void main(String[] args) {
+
+            // Contoh variabel X (misal suhu)
+            float suhuF = 36.678912f;     // pakai float
+            double suhuD = 36.678912;     // pakai double
+
+            // Cetak hasil
+            System.out.println("Nilai float  : " + suhuF);
+            System.out.println("Nilai double : " + suhuD);
+        }
+    }
