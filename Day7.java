8ec22c7c51576e668097cf61de47c9a7d66b42ef
DilahNURFADILAAMIR <dilanurfadilah689@gmail.com>
2025-09-14 19:45:00 +0800

    Day7.java

---
+++ Day7.java
@@ -0,0 +1,23 @@
+public class Day7 {
+    public static void main(String[] args) {
+        // Membuat kata "سلام" (Salaam) yang berarti "Damai" / "Salam"
+        // Tugas1: Cetak Kondisi char
+        char sin = 'س';
+        char lam = 'ل';
+        char alif = 'ا';
+        char mim = 'م';
+
+        System.out.print(sin);
+        System.out.print(lam);
+        System.out.print(alif);
+        System.out.print(mim);
+        System.out.println(); // Pindah baris
+
+        //Tugas2 : Cetak Kondisi boolean
+        boolean isSenin = true;
+        boolean isAkhirPekan = false;
+
+        System.out.println("Apakah hari ini Senin? " + isSenin);
+        System.out.println("Apakah sekarang akhir pekan? " + isAkhirPekan);
+    }
+}
