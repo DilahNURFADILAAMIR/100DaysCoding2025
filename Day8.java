b2460a29b066d5a70a4087cdff0173ba8a3d75b1
DilahNURFADILAAMIR <dilanurfadilah689@gmail.com>
2025-09-15 21:19:08 +0800

    Day8.java

---
+++ Day8.java
@@ -0,0 +1,22 @@
+public class Day8 {
+
+        public static void main(String[] args) {
+            // 1. Alasan masuk ISC
+            String alasanISC = "Aku masuk ISC karena ingin menambah ilmu dan mengasah kemampuan di bidang informatika, khususnya pemrograman, selain itu menambah pengalaman organisasi";
+
+            // 2. Alasan memilih prodi Informatika
+            String alasanInformatika = "Aku memilih prodi Informatika karena memiliki ketertarikan yang besar terhadap teknologi, terutama pemrograman dan pengembangan software. Selain itu, prospek kerja di bidang ini juga sangat luas dan menjanjikan. Aku ingin mempelajari bagaimana teknologi dapat digunakan untuk menyelesaikan masalah sehari-hari serta menciptakan inovasi yang bermanfaat bagi masyarakat. Dengan masuk ke prodi Informatika, aku berharap bisa mengasah kemampuan logika, kreativitas, dan problem solving sehingga dapat lebih siap menghadapi tantangan di dunia kerja.";
+
+            // 3. Apakah merasa salah pilih jurusan atau tidak
+            String pilihanJurusan = "Aku tidak menyesal memilih jurusan ini, walaupun penuh tantangan, aku percaya hasilnya sepadan dengan usaha";
+
+            // 4. Kendala selama di Prodi Informatika terutama pada mata kuliah PBO dan DDP
+            String kendala = "Pada DDP saya merasa kesulitan ketika harus membuat program tanpa melihat contoh karena masih bingung dengan struktur dasarnya. Sedangkan pada PBO, saya sering kebingungan bagaimana cara menghubungkan beberapa class agar program berjalan sesuai yang diharapkan";
+
+            // Cetak hasil ke konsol
+            System.out.println("Alasan masuk ISC: " + alasanISC);
+            System.out.println("Alasan memilih prodi Informatika: " + alasanInformatika);
+            System.out.println("Apakah salah pilih jurusan: " + pilihanJurusan);
+            System.out.println("Kendala selama di prodi: " + kendala);
+        }
+    }
