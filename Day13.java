736f059e1c31f7202878c3817a48441e0593aabf
DilahNURFADILAAMIR <dilanurfadilah689@gmail.com>
2025-09-20 21:01:50 +0800

    Day13.java

---
+++ Day13.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+    public class Day13 {
+        public static void main(String[] args) {
+
+            Scanner input = new Scanner(System.in);
+
+            System.out.print("Masukkan nama kamu: ");
+            String nama = input.nextLine();
+
+            System.out.println("Halo, " + nama + "! Semangat belajarnya ya.");
+
+
+        }
+    }
