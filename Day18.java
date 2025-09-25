1e65ca16d7b4732d8ee5325996aa1da9ce06ff9a
DilahNURFADILAAMIR <dilanurfadilah689@gmail.com>
2025-09-25 18:41:44 +0800

    Day18.java

---
+++ Day18.java
@@ -0,0 +1,13 @@
+public class Day18 {
+
+        public static void main(String[] args) {
+            int hargaBarang = 150000;
+            double pajak = 16500.50;
+
+            double totalBayar = hargaBarang + pajak;
+
+            System.out.println("Harga Barang: " + hargaBarang);
+            System.out.println("Pajak: " + pajak);
+            System.out.println("Total Bayar: " + totalBayar);
+        }
+    }
