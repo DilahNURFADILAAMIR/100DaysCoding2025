9989368e50cea7be24182fe3b1fb1ac4a2523e2b
DilahNURFADILAAMIR <dilanurfadilah689@gmail.com>
2025-10-21 18:48:18 +0800

    Day44.java

---
+++ Day44.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Day44 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print(" Nama: ");
+        String nama = sc.nextLine();
+        System.out.print(" Mata Kuliah: ");
+        String mataKuliah = sc.nextLine();
+        System.out.print(" Nilai: ");
+        double nilai = sc.nextDouble();
+        String predikat;
+        if (nilai > 90 && nilai <= 100) {
+            predikat = " A ";
+        } else if (nilai > 80 && nilai <= 90) {
+            predikat = " B ";
+        } else if (nilai > 70 && nilai <= 80) {
+            predikat = " C ";
+        } else if (nilai > 60 && nilai <= 70) {
+            predikat = " D ";
+        } else {
+            predikat = " E ";
+        }
+        System.out.println("\n=== Hasil Pembelajaran Selama 1 Semester ===");
+        System.out.println(" Nama : " + nama);
+        System.out.println(" Pada mata kuliah " + mataKuliah + ", " + nama + " telah mendapat nilai " + predikat);
+    }
+}
