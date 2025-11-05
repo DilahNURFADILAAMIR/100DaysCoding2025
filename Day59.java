28d23827be4684d0ab510d1fa26b6025b90e6766
DilahNURFADILAAMIR <dilanurfadilah689@gmail.com>
2025-11-05 23:17:08 +0800

    Day59.java

---
+++ Day59.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Day59 {
+        public static void main(String[] args) {
+            Scanner input = new Scanner(System.in);
+            System.out.print(" Masukkan batas angka (N): " );
+            int N = input.nextInt();
+            System.out.print(" Angka Ganjil: ");
+            for (int i = 1; i <= N; i++) {
+                if (i % 2 != 0) {
+                    System.out.print(i + " ");
+                }
+            }
+            System.out.println();
+            System.out.print(" Angka Genap: ");
+            for (int i = 1; i <= N; i++) {
+                if (i % 2 == 0) {
+                    System.out.print(i + " ");
+                }
+            }}
