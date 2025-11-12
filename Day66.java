ecf644aa9ab1d0c5e1ff893706407610684a9695
DilahNURFADILAAMIR <dilanurfadilah689@gmail.com>
2025-11-12 20:32:00 +0800

    Day66.java

---
+++ Day66.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Day66 {
+        public static void main(String[] args) {
+            Scanner tes = new Scanner(System.in);
+            int N;
+            System.out.print(" Masukkan angka (N): ");
+            N = tes.nextInt();
+            for (int i = 1; i <= N; i++) {
+                System.out.println("*");
+            }
+        }
+    }
