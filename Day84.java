bdbb97801610353e80746a8b58f032f5a264bbc5
DilahNURFADILAAMIR <dilanurfadilah689@gmail.com>
2025-11-30 22:34:56 +0800

    Day84.java

---
+++ Day84.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class Day84 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Inputan : ");
+        int n = sc.nextInt();
+        System.out.println("Output : ");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " * " + i + " = " + (i * n));
+        }
+    }}
+
+
+import java.util.Scanner;
+    public class Day84 {
+        public static void main(String[] args) {
+            Scanner tes = new Scanner(System.in);
+            System.out.println("Inputan : ");
+            int N = tes.nextInt();
+            System.out.println("Output");
+            for (int i = 1; i <= N; i++) {
+                System.out.println(" Iccang ");
+
+            }
+        }
+    }
+
+
+import java.util.Scanner;
+public class Day84 {
+    public static void main(String[] args) {
+        Scanner tes = new Scanner(System.in);
+        System.out.println("Inputan: ");
+        String teks = tes.nextLine();
+        System.out.println("Output:");
+        int N = tes.nextInt();
+        for (int i = 1; i <=N ; i++) {
+            System.out.println(teks);
+
+        }
+    }
+}
