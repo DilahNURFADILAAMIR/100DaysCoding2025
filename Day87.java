c4ea1707e22114f239d7f2662159938bd8df111f
DilahNURFADILAAMIR <dilanurfadilah689@gmail.com>
2025-12-03 21:55:37 +0800

    Day87.java

---
+++ Day87.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Day87 {
+    public static void main(String[] args) {
+        String[] buku = {"Harry Potter","Lord of the Rings","Alchemist","Sherlock Holmes","Hobbit"};
+        Scanner s = new Scanner(System.in);
+        System.out.println("--- Cari Buku ---");
+        System.out.print("Masukkan judul buku: ");
+        String j = s.nextLine();
+        boolean ada = false;
+        int p = -1;
+        for(int i = 0; i < buku.length; i++) {
+            if(buku[i].equalsIgnoreCase(j)) {
+                ada = true;
+                p = i;
+                break;
+            }
+        }
+        System.out.println("\nHasil:");
+        if(ada){
+            System.out.println("Buku '" + buku[p] + "' ditemukan di indeks ke-" + p +
+                    " (tumpukan ke-" + (p+1) + ").");
+        } else {
+            System.out.println("Maaf, buku tidak ada di tumpukan");
+        }
+    }
+}
