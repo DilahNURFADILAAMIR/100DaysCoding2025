5adce57adcc14671dfdb79afa91f90c47a5ad0a2
DilahNURFADILAAMIR <dilanurfadilah689@gmail.com>
2025-12-04 20:40:19 +0800

    Day88.java

---
+++ Day88.java
@@ -0,0 +1,9 @@
+public class Day88 {
+        public static void main(String[] args) {
+            String[] kucing = {"Milo", "Oyen", "Ciko", "Luna", "Snow"};
+            System.out.println(" Daftar Nama Kucing ");
+            for (String k : kucing) {
+                System.out.println(k);
+            }
+        }
+    }
