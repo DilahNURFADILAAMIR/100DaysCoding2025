878c90d3072d2ac5d99973207b1291529490ddf6
DilahNURFADILAAMIR <dilanurfadilah689@gmail.com>
2025-12-05 22:09:40 +0800

    Day89.java

---
+++ Day89.java
@@ -0,0 +1,8 @@
+public class Day89 {
+    static void sapaDefault() {
+        System.out.println("Halo, selamat belajar Java!");
+    }
+    public static void main(String[] args) {
+        sapaDefault(); 
+    }
+}
