d76eb0753dffc2770978f36459393bac5b10d300
DilahNURFADILAAMIR <dilanurfadilah689@gmail.com>
2025-12-08 22:49:33 +0800

    Day92.java

---
+++ Day92.java
@@ -0,0 +1,8 @@
+public class Day92 {
+    static int kaliDua(int angka) {
+        return angka * 2;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hasil: " + kaliDua(7));
+    }
+}
